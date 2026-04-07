749ed8a6ad291f24f5c5e9f4f2968c47cd6401a7
zprud <zprudurpz@gmail.com>
2026-04-07 10:29:29 -0300

    setup final para Vercel, build sem erros

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-
   build: {
     target: 'es2015',
     minify: 'esbuild',
@@ -13,10 +11,9 @@ export default defineConfig({
       onwarn(warning, warn) {
         // Ignora warnings de CSS aninhado de dependências externas
         if (warning.code === 'CSS_NESTING_INVALID') return;
-        warn(warning); // mantém outros warnings normais
-      },
-    },
+        warn(warning);
+      }
+    }
   },
-
-  logLevel: 'info', // mostra warnings sem travar o build
+  logLevel: 'info'
 });
